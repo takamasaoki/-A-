fc8940651d80aeb91e4198a379261df03a55302e
ShoIwase <i.shokun1021@gmail.com>
2016-06-27 17:15:52 +0900

    更新

---
+++ J1試合結果予想システム/C班用データベース.sql
@@ -0,0 +1,46 @@
+create table teams (
+  name varchar(50),
+  id int primary key-- idは整数で主キー
+);
+
+-- データの追加
+insert into teams (name, id) values ('べガルダ仙台', 1);
+insert into teams (name, id) values ('鹿島アントラーズ', 2);
+insert into teams (name, id) values ('柏レイソル', 3);
+insert into teams (name, id) values ('浦和レッドダイヤモンズ', 4);
+insert into teams (name, id) values ('大宮アルディージャ', 5);
+insert into teams (name, id) values ('FC東京', 6);
+insert into teams (name, id) values ('川崎フロンターレ', 7);
+insert into teams (name, id) values ('横浜F・マリノス', 8);
+insert into teams (name, id) values ('湘南ベルマーレ', 9);
+insert into teams (name, id) values ('ヴァンフォーレ甲府', 10);
+insert into teams (name, id) values ('アルビレックス新潟', 11);
+insert into teams (name, id) values ('ジュビロ磐田', 12);
+insert into teams (name, id) values ('名古屋グランパス', 13);
+insert into teams (name, id) values ('ガンバ大阪', 14);
+insert into teams (name, id) values ('ヴィッセル神戸', 15);
+insert into teams (name, id) values ('サンフレッチェ広島', 16);
+insert into teams (name, id) values ('アビスパ福岡', 17);
+insert into teams (name, id) values ('サガン鳥栖', 18);
+insert into teams (name, id) values ('コンサドーレ札幌', 19);
+insert into teams (name, id) values ('モンテディオ山形', 20);
+insert into teams (name, id) values ('水戸ホーリーホック', 21);
+insert into teams (name, id) values ('ザスパクサツ群馬', 22);
+insert into teams (name, id) values ('ジェフユナイテッド千葉',23);
+insert into teams (name, id) values ('東京ヴェルディ', 24);
+insert into teams (name, id) values ('FC町田ゼルビア', 25);
+insert into teams (name, id) values ('横浜FC', 26);
+insert into teams (name, id) values ('松本山雅FC', 27);
+insert into teams (name, id) values ('ツエーゲン金沢', 28);
+insert into teams (name, id) values ('FC岐阜', 29);
+insert into teams (name, id) values ('清水エスパルス', 30);
+insert into teams (name, id) values ('京都サンガF.C.', 31);
+insert into teams (name, id) values ('セレッソ大阪', 32);
+insert into teams (name, id) values ('ファジアーノ岡山', 33);
+insert into teams (name, id) values ('レノファ山口', 34);
+insert into teams (name, id) values ('徳島ヴォルティス', 35);
+insert into teams (name, id) values ('カマタマーレ讃岐', 36);
+insert into teams (name, id) values ('愛媛FC', 37);
+insert into teams (name, id) values ('ギラヴァンツ北九州', 38);
+insert into teams (name, id) values ('V・ファーレン長崎', 39);
+insert into teams (name, id) values ('ロアッソ熊本', 40);
