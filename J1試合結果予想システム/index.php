a5d903d80e8bf44849c823c55b4fc317c41acb52
ShoIwase <i.shokun1021@gmail.com>
2016-07-08 06:06:31 +0900

    更新

--- J1試合結果予想システム/index.php
+++ J1試合結果予想システム/index.php
@@ -1,9 +1,34 @@
+<?php
+try{
+    $pdo = new PDO(
+        'mysql:host=192.168.0.1;dbname=cv45224_ag4;charset=utf8',
+        'cv45224_ag4',
+        'peZC9M5W'
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+}catch(PDOException $Exception){
+    die('接続エラー：' .$Exception->getMessage());
+}
+
+try{
+    $year = $_GET['year'];
+    $section = $_GET['section'];
+    $sql = "SELECT * FROM schedule WHERE year='{$year}' AND section='{$section}'";
+  
+    $stmh = $pdo->prepare($sql);
+    $stmh->execute();
+}catch(PDOException $Exception){
+    die('接続エラー：' .$Exception->getMessage());
+}
+?>
 <html>
 <head>
+<meta charset='utf-8' />
 <title>ユーザー画面</title>
 <style type="text/css">
 body{
-zoom: 150%;
+zoom: 100%;
 }
 td.cell {
 text-align: center;
@@ -19,38 +44,65 @@ width: 170px;
 }
 </style>
 </head>
-<center>
+
 <body>
+  <form action="index.php" method="get">
+    <input type="hidden" name="year" value="2016">
+ 第
+  <select name="section">
+<?php optionLoop('1', '34');?>
+</select>
+節
+<?php
+//セレクトオプションのループ設定
+function optionLoop($start, $end){	
+	for($i = $start; $i <= $end; $i++){
+		echo "<option value=\"{$i}\">{$i}</option>";
+	}
+}
+?>
+  <br>
+<input type='submit' value='決定'>
+    </form>
+<center>
 
 <table>
 <tr>
 <td class="cell"></td>
 <td class="title">HOME</td>
-<td class="cell">
-<select>
-<OPTION value="1">第1節</OPTION>
-</select></td>
+<td class="cell"></td>
 <td class="title">AWAY</td>
 <td class="cell"></td>
 <td class="weather" align=center>天気</td>
 </tr>
+
 <tr>
+<?php
+    while($table = $stmh->fetch(PDO::FETCH_ASSOC)){
+?>
 <td class="cell">勝利</td>
-<td class="cell">チーム</td>
+<td class="cell"><?php echo(htmlspecialchars($table['home'])); ?></td>
 <td class="cell"></td>
-<td class="cell">チーム</td>
-<td class="cell"></td>
+<td class="cell"><?php echo(htmlspecialchars($table['away'])); ?></td>
+<td class="cell">勝利</td>
 <td class="weather">
-<input type="radio" value="晴れ" checked> 晴れ
-<input type="radio" value="雨"> 雨
-<input type="radio" value="曇り"> 曇り</td>
+<input type="radio" name="q1" value="晴れ" checked> 晴れ
+<input type="radio" name="q1" value="雨"> 雨
+<input type="radio" name="q1" value="曇り"> 曇り</td>
 </tr>
+<?php
+    }
+    $pdo = null;
+?>
 </table>
 <br>
 
 <form>
-<input type='submit' value='送信'>
+<input type='submit' value='予測開始'>
 </form>
+
+ 
 </body>
-</CENTER>
+</center>
+    <a href="login.php" ><input type='submit' value='管理者画面'></a>
 </html>
--- J1試合結果予想システム/insert.php
+++ J1試合結果予想システム/insert.php
@@ -1,9 +1,35 @@
-<html>
+<?php
+try{
+    $pdo = new PDO(
+        'mysql:host=192.168.0.1;dbname=cv45224_ag4;charset=utf8',
+        'cv45224_ag4',
+        'peZC9M5W'
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+}catch(PDOException $Exception){
+    die('接続エラー：' .$Exception->getMessage());
+}
+
+try{
+    $year = $_GET['year'];
+    $section = $_GET['section'];
+    $sql = "SELECT * FROM schedule WHERE year='{$year}' AND section='{$section}'";
+  
+    $stmh = $pdo->prepare($sql);
+    $stmh->execute();
+}catch(PDOException $Exception){
+    die('接続エラー：' .$Exception->getMessage());
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
 <head>
+  <meta charset='utf-8' />
 <title>管理者画面</title>
 <style type="text/css">
 body{
-zoom: 150%;
+zoom: 100%;
 align: center;
 }
 td.cell {
@@ -18,60 +44,72 @@ border: 1px solid;
 td.weather{
 width: 170px;
 }
-td.error{
-color: red}
 </style>
 </head>
 
 <body>
-<table>
-<td>
-<select>
-<OPTION value="1">2016年</OPTION>
+
+<form action="insert.php" method="get">
+<select name="year">
+<?php optionLoop('2011', date('Y'));?>
 </select>
-</td>
-<td>
-<select>
-<OPTION value="1">第1節</OPTION>
+年
+<br>
+<select name="section">
+<?php optionLoop('1', '34');?>
 </select>
-</td>
-</table>
+節
+<?php
+//セレクトオプションのループ設定
+function optionLoop($start, $end){	
+	for($i = $start; $i <= $end; $i++){
+		echo "<option value=\"{$i}\">{$i}</option>";
+	}
+}
+?>
+  <br>
+    <input type='submit' value='決定'></form>
 <center>
 <table>
-<tr>
-<td class="error"></td>
+<td></td>
 <td class="title">HOME<div></td>
 <td class="cell">勝敗</td>
 <td class="title">AWAY</td>
 <td class="weather" align=center>天気</td>
-</tr>
 <tr>
-<td class="error">エラー用</td>
+  
+  <?php
+    while($table = $stmh->fetch(PDO::FETCH_ASSOC)){
+  ?>
+  </tr>
+
+<td></td>
 <td class="cell">
-<select>
-<OPTION value="1">チーム名</OPTION>
-</select>
+<?php echo(htmlspecialchars($table['home'])); ?>
 </td>
 <td class="cell">
-<select>
-<OPTION value="1">勝利</OPTION>
-<OPTION value="1">敗北</OPTION>
-<OPTION value="1">引き分け</OPTION>
+<form action="insert.php" method="get">
+  <select name='result'>
+<OPTION value="勝利">勝利</OPTION>
+<OPTION value="敗北">敗北</OPTION>
+<OPTION value="引き分け">引き分け</OPTION>
+<OPTION value="無試合">無試合</OPTION>
 </select>
 </td>
+
 <td class="cell">
-<select>
-<OPTION value="1">チーム名</OPTION>
-</td>
-</select>
+<?php echo(htmlspecialchars($table['away'])); ?>
+  </td>
 <td class="weather">
-<input type="radio" value="晴れ" checked> 晴れ
-<input type="radio" value="雨"> 雨
-<input type="radio" value="曇り"> 曇り</td>
-</tr>
+<input type="radio" name="q1" value="晴れ" checked> 晴れ
+<input type="radio" name="q1" value="雨"> 雨
+<input type="radio" name="q1" value="曇り"> 曇り</td>
+<?php
+    }
+    $pdo = null;
+?>
 </table>
 <br>
-<form>
 <input type='submit' value='送信'>
 </form>
 </body>
